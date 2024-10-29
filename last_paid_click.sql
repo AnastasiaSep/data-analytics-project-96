ee646ca06a73500054c45bc497ecd18bc13a9a79
Anastasia <nastyadelonge_1@mail.ru>
2024-10-29 17:35:21 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,46 +1,46 @@
----- 2 шаг финальная версия
-
-
 WITH last_paid_sessions AS (
-    SELECT 
+    SELECT
         s.visitor_id,
         s.visit_date,
         s.source,
         s.medium,
         s.campaign,
-        ROW_NUMBER() OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC) AS rn
-    FROM 
-        sessions s
+        ROW_NUMBER()
+            OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC)
+        AS rn
+    FROM
+        sessions AS s
 
-    WHERE 
-        s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'))
+    WHERE
+        s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+)
 
-SELECT 
-	l.visitor_id,
-	--to_char(s.visit_date, 'YYYY-MM-DD') as visit_date,
-	s.visit_date as visit_date,
-	s.source AS utm_source,
+SELECT
+    l.visitor_id,
+    --to_char(s.visit_date, 'YYYY-MM-DD') as visit_date,
+    s.visit_date,
+    s.source AS utm_source,
     s.medium AS utm_medium,
     s.campaign AS utm_campaign,
     l.lead_id,
     --to_char(l.created_at, 'YYYY-MM-DD') as created_at,
-    l.created_at as created_at,
+    l.created_at,
     l.amount,
-   l.closing_reason,
+    l.closing_reason,
     l.status_id
-    
-FROM 
-    last_paid_sessions s
-LEFT JOIN leads l ON s.visitor_id = l.visitor_id and 
-    l.created_at >= s.visit_date 
-WHERE 
+
+FROM
+    last_paid_sessions AS s
+LEFT JOIN leads AS l
+    ON
+        s.visitor_id = l.visitor_id
+        AND s.visit_date <= l.created_at
+WHERE
     s.rn = 1  -- Выбираем последний платный клик для каждого посетителя
-ORDER BY 
-    l.amount DESC NULLS LAST, 
-    s.visit_date ASC, 
-    s.source ASC, 
-    s.medium ASC, 
-    s.campaign asc
-limit 10;
-   
- 
+ORDER BY
+    l.amount DESC NULLS LAST,
+    s.visit_date ASC,
+    s.source ASC,
+    s.medium ASC,
+    s.campaign ASC
+LIMIT 10;
