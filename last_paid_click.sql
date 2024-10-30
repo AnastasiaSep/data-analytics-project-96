5ac93fb7f157fdf86dccf70f689a47e38e2c2b25
Anastasia <nastyadelonge_1@mail.ru>
2024-10-30 17:31:54 +0300

    Update last_paid_click.sql

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -5,8 +5,7 @@ WITH last_paid_sessions AS (
         s.source,
         s.medium,
         s.campaign,
-        ROW_NUMBER() OVER (PARTITION BY s.visitor_id ORDER BY 
-        s.visit_date DESC) AS rn
+        ROW_NUMBER() OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC) AS rn
     FROM
         sessions AS s
     WHERE
